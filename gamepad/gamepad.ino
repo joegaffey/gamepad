4ae26ece606667228302639f8ec50af938e63316
Joe Gaffey <joe@joegaffey.com>
2017-08-24 14:24:56 +0100

    Changed to map (2D array) based data structures for easier configuration.

--- gamepad/gamepad.ino
+++ gamepad/gamepad.ino
@@ -1,9 +1,33 @@
 #include <Joystick.h>
 
+//Buttons defined according to https://www.w3.org/TR/gamepad/
+#define BUTTON_0        0
+#define BUTTON_1        1
+#define BUTTON_2        2
+#define BUTTON_3        3
+#define SHOULDER_LEFT   4
+#define SHOULDER_RIGHT  5
+#define TRIGGER_LEFT    6
+#define TRIGGER_RIGHT   7
+#define START           8
+#define SELECT          9
+#define THUMB_LEFT      10
+#define THUMB_RIGHT     11
+#define HAT_UP          12
+#define HAT_DOWN        13
+#define HAT_LEFT        14
+#define HAT_RIGHT       15
+#define HOME            16
+
+#define X_AXIS_PIN      A2 // Pin 20
+#define Y_AXIS_PIN      A1 // Pin 19
+
 Joystick_ Joystick;
 
 void setup() {
-  // Initialize Button Pins
+  // Initialize Button Pins - comment out  any unused
+//   pinMode(0, INPUT_PULLUP); 
+//   pinMode(1, INPUT_PULLUP);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
@@ -13,6 +37,13 @@ void setup() {
   pinMode(8, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
+//   pinMode(14, INPUT_PULLUP);
+//   pinMode(15, INPUT_PULLUP);
+//   pinMode(16, INPUT_PULLUP);
+//   pinMode(18, INPUT_PULLUP);
+//   pinMode(19, INPUT_PULLUP);
+//   pinMode(20, INPUT_PULLUP);
+//   pinMode(21, INPUT_PULLUP);
 
   // Initialize Joystick Library
   Joystick.begin();
@@ -21,28 +52,58 @@ void setup() {
   Joystick.setYAxisRange(1024, 0);
 }
 
-// Last state of the button
-int lastButtonState[9] = {0,0,0,0,0,0,0,0,0};
+//Edit button to pin mapping according to your hardware - use -1 if unused
+int pinToButtonMap[20][2] =  {{BUTTON_0,        9},
+                              {BUTTON_1,        8},
+                              {BUTTON_2,        7},
+                              {BUTTON_3,        6},
+                              {SHOULDER_LEFT,   4},
+                              {SHOULDER_RIGHT,  5},
+                              {TRIGGER_LEFT,    -1},
+                              {TRIGGER_RIGHT,   -1},
+                              {START,           3},
+                              {SELECT,          2},
+                              {THUMB_LEFT,      10},
+                              {THUMB_RIGHT,     -1},
+                              {HAT_UP,          -1},
+                              {THUMB_LEFT,      -1},
+                              {HAT_UP,          -1},
+                              {HAT_DOWN,        -1},
+                              {HAT_LEFT,        -1},
+                              {HAT_RIGHT,       -1},
+                              {HOME,            -1}};
 
-// Array index is joystick button number
-int pinToButtonMap[9] = {9,8,7,6,4,5,10,3,2};
+int buttonStateMap[20][2] =  {{BUTTON_0, 0},
+                              {BUTTON_1, 0},
+                              {BUTTON_2, 0},
+                              {BUTTON_3, 0},
+                              {SHOULDER_LEFT, 0},
+                              {SHOULDER_RIGHT, 0},
+                              {TRIGGER_LEFT, 0},
+                              {TRIGGER_RIGHT, 0},
+                              {START, 0},
+                              {SELECT, 0},
+                              {THUMB_LEFT, 0},
+                              {THUMB_RIGHT, 0},
+                              {HAT_UP, 0},
+                              {THUMB_LEFT, 0},
+                              {HAT_UP, 0},
+                              {HAT_DOWN, 0},
+                              {HAT_LEFT, 0},
+                              {HAT_RIGHT, 0},
+                              {HOME, 0}};
 
 void loop() {
-
-  // Read pin values
-  for (int index = 0; index < 9; index++)
-  {
-    int currentButtonState = !digitalRead(pinToButtonMap[index]);
-    if (currentButtonState != lastButtonState[index])
-    {
-      Joystick.setButton(index, currentButtonState);
-      lastButtonState[index] = currentButtonState;
+  for (int i = 0; i < 20; i++)  {
+    if(pinToButtonMap[i][1] > -1) {
+      int currentState = !digitalRead(pinToButtonMap[i][1]);
+      if (currentState != buttonStateMap[i][1]) { // Detect button state change
+        Joystick.setButton(i, currentState);
+        buttonStateMap[i][1] = currentState;
+      }
     }
   }
-
-  Joystick.setXAxis(analogRead(A2));
-  Joystick.setYAxis(analogRead(A1));
-
+  Joystick.setXAxis(analogRead(X_AXIS_PIN));
+  Joystick.setYAxis(analogRead(Y_AXIS_PIN));
   delay(50);
 }
-
